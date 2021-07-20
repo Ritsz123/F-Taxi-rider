04b67286c8e7b5b8224e7f57512f5e5921eae586
ritesh <riteshkhadse12@gmail.com>
2021-07-20 11:40:02 +0530

    search screen ui changes

--- lib/Screens/searchScreen.dart
+++ lib/Screens/searchScreen.dart
@@ -53,131 +53,140 @@ class _SearchScreenState extends State<SearchScreen> {
     pickupTextController.text = address;
     setDestinationFocus();
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        leading: GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Icon(
-            Icons.arrow_back,
-            size: 30,
-            color: Colors.black,
+      appBar: _appBar(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0.7, 0.7),
+                      spreadRadius: .5,
+                      blurRadius: .5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/pickicon.png',
+                          height: 16,
+                          width: 16,
+                        ),
+                        18.widthBox,
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: MyColors.colorLightGrayFair,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: TextField(
+                              controller: pickupTextController,
+                              decoration: InputDecoration(
+                                hintText: "Pick up Location",
+                                fillColor: MyColors.colorLightGrayFair,
+                                filled: true,
+                                border: InputBorder.none,
+                                isDense: true,
+                                contentPadding: EdgeInsets.only(
+                                  left: 10,
+                                  top: 8,
+                                  bottom: 8,
+                                ),
+                              ),
+                            ).p2(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    10.heightBox,
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/desticon.png',
+                          height: 16,
+                          width: 16,
+                        ),
+                        18.widthBox,
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: MyColors.colorLightGrayFair,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: TextField(
+                              onChanged: (value) {
+                                getSearchSuggestions(value);
+                              },
+                              focusNode: focusDestination,
+                              controller: destinationTextController,
+                              decoration: InputDecoration(
+                                hintText: "Where to?",
+                                fillColor: MyColors.colorLightGrayFair,
+                                filled: true,
+                                border: InputBorder.none,
+                                isDense: true,
+                                contentPadding: EdgeInsets.only(
+                                  left: 10,
+                                  top: 8,
+                                  bottom: 8,
+                                ),
+                              ),
+                            ).p2(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ).pOnly(top: 16, left: 16, right: 16, bottom: 25),
+              ),
+              destinationSuggestionList.length > 0
+                ? _suggestionTileList()
+                : Container()
+            ],
           ),
         ),
-        centerTitle: true,
-        title: "Set Destination".text.black.size(22).fontFamily('Brand-Bold').make(),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-//              height: 210,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    offset: Offset(0.7, 0.7),
-                    spreadRadius: .5,
-                    blurRadius: .5,
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/images/pickicon.png',
-                        height: 16,
-                        width: 16,
-                      ),
-                      18.widthBox,
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: MyColors.colorLightGrayFair,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: TextField(
-                            controller: pickupTextController,
-                            decoration: InputDecoration(
-                              hintText: "Pick up Location",
-                              fillColor: MyColors.colorLightGrayFair,
-                              filled: true,
-                              border: InputBorder.none,
-                              isDense: true,
-                              contentPadding: EdgeInsets.only(
-                                left: 10,
-                                top: 8,
-                                bottom: 8,
-                              ),
-                            ),
-                          ).p2(),
-                        ),
-                      ),
-                    ],
-                  ),
-                  10.heightBox,
-                  Row(
-                    children: [
-                      Image.asset(
-                        'assets/images/desticon.png',
-                        height: 16,
-                        width: 16,
-                      ),
-                      18.widthBox,
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: MyColors.colorLightGrayFair,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: TextField(
-                            onChanged: (value) {
-                              getSearchSuggestions(value);
-                            },
-                            focusNode: focusDestination,
-                            controller: destinationTextController,
-                            decoration: InputDecoration(
-                              hintText: "Where to?",
-                              fillColor: MyColors.colorLightGrayFair,
-                              filled: true,
-                              border: InputBorder.none,
-                              isDense: true,
-                              contentPadding: EdgeInsets.only(
-                                left: 10,
-                                top: 8,
-                                bottom: 8,
-                              ),
-                            ),
-                          ).p2(),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ).pOnly(top: 16, left: 16, right: 16, bottom: 25),
-            ),
-            destinationSuggestionList.length > 0
-                ? ListView.separated(
-                    padding: EdgeInsets.all(0),
-                    itemBuilder: (context, index) {
-                      return SuggestionTile(
-                        suggestion: destinationSuggestionList[index],
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) =>
-                        MyDivider(),
-                    itemCount: destinationSuggestionList.length,
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                  ).pSymmetric(v: 8, h: 16)
-                : Container()
-          ],
+    );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
+      leading: GestureDetector(
+        onTap: () => Navigator.pop(context),
+        child: Icon(
+          Icons.arrow_back,
+          size: 30,
+          color: Colors.black,
         ),
       ),
+      centerTitle: true,
+      title: "Set Destination".text.black.size(22).fontFamily('Brand-Bold').make(),
+    );
+  }
+
+  Widget _suggestionTileList() {
+    return ListView.separated(
+      padding: EdgeInsets.all(8),
+      itemBuilder: (context, index) {
+        return SuggestionTile(
+          suggestion: destinationSuggestionList[index],
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) =>
+          MyDivider(),
+      itemCount: destinationSuggestionList.length,
+      shrinkWrap: true,
+      physics: ClampingScrollPhysics(),
     );
   }
 }
